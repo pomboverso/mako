9259da5ff51fbcfbb0bf1fea559d1a8c6a41f8c7
Pombo <github.5yeil@simplelogin.fr>
2026-04-20 14:54:03 -0700

    re-calculate insets after the bar has change visibility

--- app/src/main/java/com/rama/mako/CsActivity.kt
+++ app/src/main/java/com/rama/mako/CsActivity.kt
@@ -49,7 +49,7 @@ abstract class CsActivity : Activity() {
         FontManager.applyFont(this, root)
     }
 
-    protected fun updateSystemBars() {
+    protected fun updateSystemBars(root: View) {
         if (prefs.isSystemBarVisible()) {
             window.decorView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -60,12 +60,14 @@ abstract class CsActivity : Activity() {
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
+        root.requestApplyInsets()
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            updateSystemBars()
+            val root = findViewById<View>(android.R.id.content)
+            updateSystemBars(root)
         }
     }
 
